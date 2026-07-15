0163f6b40b595ba9fc745abecc7db33b0af44c77
ali-shokor <alishokermana@gmail.com>
2026-07-16 01:34:27 +0300

    1.8.3

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/UFOQ-MVP/",
+  base: "/IMKAN-MVP/",
   server: {
     port: 3000,
     open: true,
